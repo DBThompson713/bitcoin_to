2eb196e1707c7e618f4af01c536878ce1cd648a1
DBThompson713 <dbthompson.au@gmail.com>
2019-09-03 14:54:08 +1000

    added intro image and images folder

--- index.rb
+++ index.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'json'
 require 'io/console' 
+require 'catpix'
 
 def key_to_continue                                                                                                           
     print "press any key to start!"                                                                                                    
@@ -75,7 +76,17 @@ end
 
 # Logo will go here to replace the welcome message
 system("clear")
-puts "Bitcoin to: ?"
+
+
+Catpix::print_image "./images/bitcoin_to_logo2.jpg",
+  :limit_x => 0.75,
+  :limit_y => 0,
+  :center_x => true,
+  :center_y => true,
+  :bg => "white",
+  :bg_fill => false,
+  :resolution => "high"
+
 key_to_continue
 
 gameplay

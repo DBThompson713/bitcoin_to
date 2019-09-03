69b067893f926ffb00e186aa46dda0e9db4eba5e
DBThompson713 <dbthompson.au@gmail.com>
2019-09-03 11:43:52 +1000

    added array of questions

--- index.rb
+++ index.rb
@@ -4,6 +4,15 @@ require 'io/console'
 
 correct = false
 answer = nil
+questions = [
+    ["How many bigmacs can you buy with 1 bitcoin?", 5.5],
+    ["How many 2004 Holden Commodores can you buy with 1 btc?", 4500],
+    ["How many pets goats could you buy with 1 bitcoin?", 180],
+    ["How many packets of Tim Tams could you buy with 1 bitcoin", 3.65],
+    ["How many XXXXX drops could you get with 1 bitcoin", 000]
+]
+
+
 
 def key_to_continue                                                                                                           
     print "press any key to start!"                                                                                                    
@@ -16,3 +25,11 @@ def self.blockchain
     json = JSON.parse(Net::HTTP.get(uri))
     json["AUD"]["last"]
 end
+
+# Logo will go here to replace the welcome message
+
+system("clear")
+puts "Bitcoin to: ?"
+key_to_continue
+
+

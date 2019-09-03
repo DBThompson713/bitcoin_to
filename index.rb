9e469d83742316fddb77312a3b570204e94a05d6
DBThompson713 <dbthompson.au@gmail.com>
2019-09-03 12:21:25 +1000

    question loop built

--- index.rb
+++ index.rb
@@ -3,13 +3,15 @@ require 'json'
 require 'io/console' 
 
 correct = false
+q_picker = rand(5)
 answer = nil
+correct = false
 questions = [
     ["How many bigmacs can you buy with 1 bitcoin?", 5.5],
     ["How many 2004 Holden Commodores can you buy with 1 btc?", 4500],
     ["How many pets goats could you buy with 1 bitcoin?", 180],
     ["How many packets of Tim Tams could you buy with 1 bitcoin", 3.65],
-    ["How many XXXXX drops could you get with 1 bitcoin", 000]
+    ["How many XXXXX drops could you get with 1 bitcoin", 3.99 ]
 ]
 
 
@@ -20,7 +22,7 @@ def key_to_continue
     print "            \r"                                                                                                             
 end 
 
-def self.blockchain
+def blockchain
     uri = URI('https://blockchain.info/ticker')
     json = JSON.parse(Net::HTTP.get(uri))
     json["AUD"]["last"]
@@ -31,5 +33,33 @@ end
 system("clear")
 puts "Bitcoin to: ?"
 key_to_continue
+
+
+while !correct
+    system("clear")
+    number_of_items = ((blockchain.to_i)/questions[q_picker][1]).round
+    p questions[q_picker][0]
+    puts "\n"
+    answer = gets.to_i
+    puts number_of_items
+    # puts answer
+    if answer < number_of_items
+        puts "too low! Try again!"
+        puts "\n"
+        next
+    elsif answer > number_of_items
+        puts "too high! Try again"
+        puts "\n"
+
+    elsif answer == number_of_items
+        puts "Wow thats right!"
+        correct = true
+    else
+       puts "That's not a number, try again!" 
+    end
+    key_to_continue
+end
+
+
 
 

829f306bfd97c2c00e1a9f0077dc90e915787e78
DBThompson713 <dbthompson.au@gmail.com>
2019-09-03 15:15:40 +1000

    centered text, corrected layout

--- index.rb
+++ index.rb
@@ -4,7 +4,7 @@ require 'io/console'
 require 'catpix'
 
 def key_to_continue                                                                                                           
-    print "press any key to start!"                                                                                                    
+    print "press any key to start!".center(175)                                                                                                 
     STDIN.getch                                                                                                              
     print "            \r"                                                                                                             
 end 
@@ -16,9 +16,9 @@ def blockchain
 end
 
 def play_again
-    puts "Would you like to play again?"
-    puts "       yes    or     no      "
-    play_again_answer = gets.chomp
+    puts "Would you like to play again?".center(175)     
+    puts "       yes    or     no      ".center(175)     
+    play_again_answer = gets.chomp.downcase
     
     if play_again_answer == "yes"
         gameplay
@@ -35,40 +35,46 @@ def gameplay
     q_picker = rand(5)
     iteration = 5
     questions = [
-    ["How many bigmacs can you buy with 1 bitcoin?", 5.5],
-    ["How many 2004 Holden Commodores can you buy with 1 btc?", 4500],
-    ["How many pets goats could you buy with 1 bitcoin?", 180],
-    ["How many packets of Tim Tams could you buy with 1 bitcoin", 3.65],
-    ["How many XXXXX drops could you get with 1 bitcoin", 3.99 ]
+    ["How many bigmacs can you buy with 1 bitcoin?".center(175)     , 5.5],
+    ["How many 2004 Holden Commodores can you buy with 1 btc?".center(175)     , 4500],
+    ["How many pets goats could you buy with 1 bitcoin?".center(175)     , 180],
+    ["How many packets of Tim Tams could you buy with 1 bitcoin".center(175)     , 3.65],
+    ["How many Nintendo Switches can you buy with 1 bitcoin".center(175)     , 399 ]
     ]
 
     system("clear")
 
     while !correct
         number_of_items = ((blockchain.to_i)/questions[q_picker][1]).round
+        puts "\n"
+        puts "\n"
         p questions[q_picker][0]
         puts "\n"
         answer = gets.to_i
         iteration -=1
-        puts number_of_items
-        puts iteration
+        puts "you have #{iteration} guesses left!".center(175)
         # puts answer
         if iteration ==0
-            puts "You have no more guesses left"
+            puts "\n"
+            puts "\n"
+            puts "You have no more guesses left".center(175)
+            puts " the correct answer was #{number_of_items}".center(175)
+            puts "\n"
             play_again
         elsif answer < number_of_items
-            puts "too low! Try again!"
+            puts "too low! Try again!".center(175)
             puts "\n"
             next
         elsif answer > number_of_items
-            puts "too high! Try again"
+            puts "too high! Try again".center(175)
             puts "\n"
 
         elsif answer == number_of_items
-            puts "Wow thats right!"
+            puts "Wow thats right!".center(175)
             correct = true
+            break
         else
-        puts "That's not a number, try again!" 
+        puts "That's not a number, try again!".center(175)
         end
     end
 end
@@ -87,8 +93,9 @@ Catpix::print_image "./images/bitcoin_to_logo2.jpg",
   :bg_fill => false,
   :resolution => "high"
 
-key_to_continue
+  key_to_continue
 
-gameplay
+
+  gameplay
 
 play_again

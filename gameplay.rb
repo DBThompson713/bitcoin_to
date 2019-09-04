8b53f6723c834f2e0806c7f6c7ec53047647cde8
DBThompson713 <dbthompson.au@gmail.com>
2019-09-04 11:22:09 +1000

    improved print out, added btc thumbs up image on win

--- gameplay.rb
+++ gameplay.rb
@@ -3,11 +3,11 @@ def gameplay
     q_picker = rand(5)
     iteration = 7
     questions = [
-    ["How many Big Macs can you buy with 1 bitcoin?".center(175)     , 5.5,"images/big_mac.jpg"],
-    ["How many 2004 Holden Commodores can you buy with 1 bitcoin?".center(175)     , 4500,"images/commodore.jpg"],
-    ["How many pets goats could you buy with 1 bitcoin?".center(175)     , 180, "images/new_goat.jpg"],
-    ["How many packets of Tim Tams could you buy with 1 bitcoin".center(175)     , 3.65, "images/timtam.jpg"],
-    ["How many Nintendo Switches can you buy with 1 bitcoin".center(175)     , 399, "images/switch_prop.jpg" ]
+    ["How many Big Macs can you currently buy with 1 bitcoin?".center(182)     , 5.5,"images/big_mac.jpg"],
+    ["How many 2004 Holden Commodores can you currently buy with 1 bitcoin?".center(175)     , 4500,"images/commodore.jpg"],
+    ["How many pets goats could you currently buy with 1 bitcoin?".center(175)     , 180, "images/new_goat.jpg"],
+    ["How many packets of Tim Tams could you currently buy with 1 bitcoin".center(185)     , 3.65, "images/timtam.jpg"],
+    ["How many Nintendo Switches can you currently buy with 1 bitcoin".center(175)     , 399, "images/switch_prop.jpg" ]
     ]
 
     system("clear")
@@ -26,6 +26,8 @@ def gameplay
         number_of_items = ((blockchain.to_i)/questions[q_picker][1]).round
     
         puts "\n"
+        
+        
 
         p questions[q_picker][0]
         puts "\n"
@@ -49,8 +51,8 @@ def gameplay
             :bg => "black",
             :bg_fill => false,
             :resolution => "high"
-
-            puts "too low! Try again!".center(175)
+            puts "\n"
+            puts "#{answer} is too low! Try again!".center(175)
             puts "you have #{iteration} guesses left!".center(175)
             puts "\n"
         elsif answer > number_of_items
@@ -64,11 +66,22 @@ def gameplay
             :bg_fill => false,
             :resolution => "high"
 
-            puts "too high! Try again".center(175)
+            puts "\n"
+            puts "#{answer} is too high! Try again".center(175)
             puts "you have #{iteration} guesses left!".center(175)
             puts "\n"
         elsif answer == number_of_items
             system("clear")
+
+            Catpix::print_image "./images/btc_thumb.jpg",
+            :limit_x => 0.3,
+            :limit_y => 0,
+            :center_x => true,
+            :center_y => false,
+            :bg => "black",
+            :bg_fill => true,
+            :resolution => "high"
+
             spacing
             puts "You got it right!!".center(175)
             puts "\n"
